6918fcb3e229738c79845fc339100296158cedd9
GACC-UY <guillermo.cabrera@enjoy.cl>
2025-11-11 13:59:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from io import BytesIO
+import re
+import unicode
 
 # ---------------------------------------------------------
 # PAGE CONFIG
